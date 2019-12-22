2e04009319e546f2d6242d725baac48d69aa417c
Albara Ramli <arramli@ucdavis.edu>
2019-12-21 22:32:03 -0800

    Update local.sh

--- sh/local.sh
+++ sh/local.sh
@@ -11,7 +11,9 @@ fi
 CONFIG_FILE="$MY_PATH/../"config.txt
 DATA_PATH="$MY_PATH/../data/"
 LOCAL_PATH="$DATA_PATH"local.html
+LOCAL_IMG="$DATA_PATH"internet.png
 
 if [ "$(sed '3q;d' "$CONFIG_FILE")" != 'mic' ]; then
-curl -L -o "$LOCAL_PATH" "$(sed '4q;d' "$CONFIG_FILE")local.php?city=$(sed '1q;d' "$CONFIG_FILE")&icdx=1&note_show=0&remain_show=0&time_show=0&refresh_stop=1&arrow_show=0&date_show=0&current_show=0"
+curl -L -o "$LOCAL_PATH" "$(sed '4q;d' "$CONFIG_FILE")local.php?size=$(sed '5q;d' "$CONFIG_FILE")&city=$(sed '1q;d' "$CONFIG_FILE")&icdx=1&note_show=0&remain_show=0&time_show=0&refresh_stop=1&arrow_show=0&date_show=0&current_show=0"
+curl -L -o "$LOCAL_IMG" "$(sed '4q;d' "$CONFIG_FILE")internet.png"
 fi
