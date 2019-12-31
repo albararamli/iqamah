c30a28a8ce41567acd55bcc5afd25aadee7c8261
Albara Ramli <arramli@ucdavis.edu>
2019-12-30 17:48:39 -0800

    Update install.sh

--- sh/install.sh
+++ sh/install.sh
@@ -46,8 +46,8 @@ echo "#00 * * * * "$SH_PATH"MouseMove.sh" >> mycron
 crontab mycron
 rm mycron
 
-#AUTO_FILE=/etc/xdg/lxsession/LXDE-pi/autostart
-AUTO_FILE=~/.config/lxsession/LXDE-pi/autostart
+AUTO_FILE=/etc/xdg/lxsession/LXDE-pi/autostart # official Raspbian Buster with desktop and recommended software
+#AUTO_FILE=~/.config/lxsession/LXDE-pi/autostart
 echo "@lxpanel --profile LXDE-pi" > $AUTO_FILE
 echo "@pcmanfm --desktop --profile LXDE-pi" >> $AUTO_FILE
 echo "@xscreensaver -no-splash" >> $AUTO_FILE
