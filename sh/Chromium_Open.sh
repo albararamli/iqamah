211015ac038d8897fe921550b10721e563f05b78
Albara Ramli <arramli@ucdavis.edu>
2020-10-25 00:11:50 -0700

    Update Chromium_Open.sh

--- sh/Chromium_Open.sh
+++ sh/Chromium_Open.sh
@@ -11,16 +11,18 @@ fi
 CONFIG_FILE="$MY_PATH/../"config.txt
 DATA_PATH="$MY_PATH/../data/"
 ###
-IFACE_E=eth0
+IFACE_E="eth0"
 read MAC_E </sys/class/net/$IFACE_E/address
-IFACE_W=wlan0
+IFACE_W="wlan0"
 read MAC_W </sys/class/net/$IFACE_W/address
-MACX=$IFACE_E$MAC_E$IFACE_W$MAC_W
-MACX2="${MACX//:}"
-#echo $MACX2
+MACX=${IFACE_E}${MAC_E}${IFACE_W}${MAC_W}
+#MACX2="${MACX//:}"
+MACX2=$(echo $MACX | sed 's/[:"]//g')
+#echo ${MACX2}
 ###
 echo "1" > "$DATA_PATH"chrome_status.txt
-# Open Chromium Browser Instance, if the device is not mic
+#Open Chromium Browser Instance, if the device is not mic
 if [ "$(sed '3q;d' "$CONFIG_FILE")" != 'mic' ]; then
-  chromium-browser --kiosk --incognito --no-sandbox --noerrdialogs -disable-session-crashed-bubble --disable-infobars --test-type "$(sed '4q;d' "$CONFIG_FILE")screen.php?size=$(sed '5q;d' "$CONFIG_FILE")&icdx=1&city=$(sed '1q;d' "$CONFIG_FILE")&mac=$MACX2"
+#echo "$(sed '4q;d' "$CONFIG_FILE")screen.php?size=$(sed '5q;d' "$CONFIG_FILE")&icdx=1&city=$(sed '1q;d' "$CONFIG_FILE")&mac=${MACX2}"
+chromium-browser --kiosk --incognito --no-sandbox --noerrdialogs -disable-session-crashed-bubble --disable-infobars --test-type "$(sed '4q;d' "$CONFIG_FILE")screen.php?size=$(sed '5q;d' "$CONFIG_FILE")&icdx=1&city=$(sed '1q;d' "$CONFIG_FILE")&mac=${MACX2}"
 fi
