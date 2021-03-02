b4be121a8738931433d79eb9a2eb3626c49cdcb3
Albara Ramli <arramli@ucdavis.edu>
2021-03-01 23:33:23 -0800

    Update install2.sh

--- sh/install2.sh
+++ sh/install2.sh
@@ -53,11 +53,11 @@ echo "@xscreensaver -no-splash" >> $AUTO_FILE
 echo "@point-rpi" >> $AUTO_FILE
 echo "sudo sh "$SH_PATH"MouseMove.sh" >> $AUTO_FILE
 echo "sudo sh "$SH_PATH"screen_on.sh" >> $AUTO_FILE
-echo "sh "$SH_PATH"Chromium_Open.sh" >> $AUTO_FILE  # in the 3.5 and 7 screen remove "sudo" cuz chrome requires that
+echo "sudo sh "$SH_PATH"Chromium_Open.sh" >> $AUTO_FILE  # in the 3.5 and 7 screen remove "sudo" cuz chrome requires that
 echo "sudo sh "$SH_PATH"iqamahTimeParser.sh" >> $AUTO_FILE
 echo "sudo python "$PY_PATH"screenTimeCheck.py" >> $AUTO_FILE
 echo "sudo python "$PY_PATH"mic_ini.py" >> $AUTO_FILE
 echo "sudo python "$PY_PATH"mic_listen_online.py" >> $AUTO_FILE
-echo "python "$PY_PATH"internet.py" >> $AUTO_FILE # in my 7 inch last time works only when there is no sudo here
+echo "sudo python "$PY_PATH"internet.py" >> $AUTO_FILE # in my 7 inch last time works only when there is no sudo here
 echo "sudo python "$PY_PATH"sensor.py" >> $AUTO_FILE
 
