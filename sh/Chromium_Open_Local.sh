05050aefecd556d7f86504a08e65ce10555a5c62
Albara Ramli <arramli@ucdavis.edu>
2022-06-26 13:01:52 -0700

    Update Chromium_Open_Local.sh

--- sh/Chromium_Open_Local.sh
+++ sh/Chromium_Open_Local.sh
@@ -10,7 +10,23 @@ fi
 #echo "$MY_PATH"
 CONFIG_FILE="$MY_PATH/../"config.txt
 DATA_PATH="$MY_PATH/../data/"
+###
+IFACE_E="eth0"
+read MAC_E </sys/class/net/$IFACE_E/address
+IFACE_W="wlan0"
+read MAC_W </sys/class/net/$IFACE_W/address
+MACX=${IFACE_E}${MAC_E}${IFACE_W}${MAC_W}
+#MACX2="${MACX//:}"
+MACX2=$(echo $MACX | sed 's/[:"]//g')
+echo ${MACX2}
+
+LOCAL_FILE_sbr="$DATA_PATH"sbr2.sbr
+
 LOCAL_FILE="$DATA_PATH"output.html
+if [ "$(echo ${MACX2}"PASSED" | base64)" != "$(sed '1q;d' "$LOCAL_FILE_sbr")" ]; then
+LOCAL_FILE="$DATA_PATH"block.html
+fi
+###
 
 echo "1" > "$DATA_PATH"chrome_status.txt
 # Open Chromium Browser Instance, if the device is not mic
