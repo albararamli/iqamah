5cee6e412682b48d6e9ea533f5313728c6ae87bd
Albara Ramli <arramli@ucdavis.edu>
2022-08-28 01:11:59 -0700

    Update screen_off.sh

--- sh/screen_off.sh
+++ sh/screen_off.sh
@@ -15,6 +15,7 @@ echo "0" > "$DATA_PATH"screen_status.txt
 if [ "$(sed '3q;d' "$CONFIG_FILE")" != 'mic' ]; then
 vcgencmd display_power 0
 sudo echo 1 > /sys/class/backlight/rpi_backlight/bl_power # LCD 7'' turn off with 1 tag
+xrandr --output HDMI-1 --off
 fi
 
 
