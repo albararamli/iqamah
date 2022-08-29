5e651c33ad037dae5ebe5f2ff1a8e7596a7136ea
Albara Ramli <arramli@ucdavis.edu>
2022-08-29 02:36:42 -0700

    Update local.sh

--- sh/local.sh
+++ sh/local.sh
@@ -25,10 +25,10 @@ MACX2=$(echo $MACX | sed 's/[:"]//g')
 #echo ${MACX2}
 ### 
 if [ "$(sed '3q;d' "$CONFIG_FILE")" != 'mic' ]; then
-curl -kLo "$LOCAL_PATH" "$(sed '4q;d' "$CONFIG_FILE")local.php?size=$(sed '5q;d' "$CONFIG_FILE")&city=$(sed '1q;d' "$CONFIG_FILE")&icdx=1&note_show=0&remain_show=0&time_show=0&refresh_stop=1&arrow_show=0&date_show=0&current_show=0&mac=${MACX2}"
-curl -kLo "$LOCAL_PATH_BLOCK" "$(sed '4q;d' "$CONFIG_FILE")local.php?size=$(sed '5q;d' "$CONFIG_FILE")&city=$(sed '1q;d' "$CONFIG_FILE")&icdx=1&note_show=0&remain_show=0&time_show=0&refresh_stop=1&arrow_show=0&date_show=0&current_show=0&mac=111"
-#curl -kLo "$LOCAL_IMG" "$(sed '4q;d' "$CONFIG_FILE")internet.png"
-#curl -kLo "$LOCAL_IMG2" "$(sed '4q;d' "$CONFIG_FILE")all.png"
+curl -Lo "$LOCAL_PATH" "$(sed '4q;d' "$CONFIG_FILE")local.php?size=$(sed '5q;d' "$CONFIG_FILE")&city=$(sed '1q;d' "$CONFIG_FILE")&icdx=1&note_show=0&remain_show=0&time_show=0&refresh_stop=1&arrow_show=0&date_show=0&current_show=0&mac=${MACX2}"
+curl -Lo "$LOCAL_PATH_BLOCK" "$(sed '4q;d' "$CONFIG_FILE")local.php?size=$(sed '5q;d' "$CONFIG_FILE")&city=$(sed '1q;d' "$CONFIG_FILE")&icdx=1&note_show=0&remain_show=0&time_show=0&refresh_stop=1&arrow_show=0&date_show=0&current_show=0&mac=111"
+#curl -Lo "$LOCAL_IMG" "$(sed '4q;d' "$CONFIG_FILE")internet.png"
+#curl -Lo "$LOCAL_IMG2" "$(sed '4q;d' "$CONFIG_FILE")all.png"
 ###############
 ###############
 for VARIABLE in 0 1 2 3 4 5 6 7 8 9 10    
@@ -38,7 +38,7 @@ LOCAL_PATH_EXPORT_YEAR=$(date --date=$VARIABLE" year" +%Y)
 #MACOS
 #LOCAL_PATH_EXPORT_YEAR=$(date -v+"$VARIABLE""y" +%Y)
 LOCAL_PATH_EXPORT="$DATA_PATH""$LOCAL_PATH_EXPORT_YEAR".json
-curl -kLo "$LOCAL_PATH_EXPORT" "$(sed '4q;d' "$CONFIG_FILE")export.php?city=$(sed '1q;d' "$CONFIG_FILE")&year=$LOCAL_PATH_EXPORT_YEAR&typex=json"
+curl -Lo "$LOCAL_PATH_EXPORT" "$(sed '4q;d' "$CONFIG_FILE")export.php?city=$(sed '1q;d' "$CONFIG_FILE")&year=$LOCAL_PATH_EXPORT_YEAR&typex=json"
 done
 ###############
 ###############
