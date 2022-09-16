301c59b6bbe2d13b7905f2e1223d42d43e95fdc7
Albara Ramli <arramli@ucdavis.edu>
2022-09-15 22:35:26 -0700

    Update mic_ini.py

--- py/mic_ini.py
+++ py/mic_ini.py
@@ -9,28 +9,19 @@ dir_local_mic=path_py
 #exit the file if the device is not a mic device
 if(device_type=='monitor'):
     sys.exit() 
-    
-RELAY_1=12
-RELAY_2=16#Amp
-RELAY_3=18#Mixer
-RELAY_4=22#Wireless mic
-RELAY_5=32
-RELAY_6=36
-RELAY_7=38
-RELAY_8=40#External Speaker
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(29, GPIO.OUT)#blue led
+GPIO.setup(LED_BLUE, GPIO.OUT)#blue led
 
 ##GPIO.setup(40, GPIO.OUT)#Control the Relay
 GPIO.setup(RELAY_2, GPIO.OUT)#Control the Relay
 GPIO.setup(RELAY_3, GPIO.OUT)#Control the Relay
 GPIO.setup(RELAY_4, GPIO.OUT)#Control the Relay
 
-GPIO.setup(33, GPIO.OUT)#Red led
-GPIO.setup(35, GPIO.OUT)#the Green led
-GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)#the Blue switch
-GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)#the Red switch
+GPIO.setup(LED_RED, GPIO.OUT)#Red led
+GPIO.setup(LED_GREEN, GPIO.OUT)#the Green led
+#GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)#the Blue switch
+#GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)#the Red switch
 
 status_web=-1 #blue led status 0,1
 status_local=-1 #green led status 0,1
@@ -47,15 +38,15 @@ for arg in sys.argv:
 # get the blue led status 0,1 from the file status_web.txt
 status_web=get_it(file_web) # get the blue led status 0,1 from the file status_web.txt
 #########
-GPIO.output(29,status_web) #blue led
+GPIO.output(LED_BLUE,status_web) #blue led
 do_it(status_web,file_web) # wite the blue led status in the file status_web.txt
 print ("Web-->",status_web)
 ############################################################################
 # get the relay status 0,1 (as well as green led status) from the command
 status_local=get_it(file_relay) # get the relay status 0,1 (as well as green led status) from the file status_relay.txt
 #########
-GPIO.output(33, swap_it(status_local)) #Red led
-GPIO.output(35, status_local)#the Green led
+GPIO.output(LED_RED, swap_it(status_local)) #Red led
+GPIO.output(LED_GREEN, status_local)#the Green led
 ##GPIO.output(40, status_local)#Control the Relay
 if(status_local==0):
     ###GPIO.output(RELAY_4, swap_it(status_local))#Control the Relay
