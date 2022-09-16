936dd27c0894a63ef0ba6a3f2a44224cc79c2fe8
Albara Ramli <arramli@ucdavis.edu>
2022-09-15 22:35:59 -0700

    Update fun_sbr.py

--- py/fun_sbr.py
+++ py/fun_sbr.py
@@ -77,3 +77,17 @@ device_id  =get_line_form_file(config_file,1) # 1 vs 2 vs 3
 device_type=get_line_form_file(config_file,2) # monitor vs mic vs ''
 app_url    =get_line_form_file(config_file,3) # http://albara.ramli.net/iqamah/
 screen_size    =get_line_form_file(config_file,4) # 27 vs 7
+
+
+RELAY_1=12
+RELAY_2=16#Amp
+RELAY_3=18#Mixer
+RELAY_4=22#Wireless mic
+RELAY_5=32
+RELAY_6=36
+RELAY_7=38
+RELAY_8=40#External Speaker
+
+LED_RED=33
+LED_BLUE=29
+LED_GREEN=35
