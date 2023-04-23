3f72e43ab2c5d8e01c9adeba82f94251a0bc86f8
Albara Ramli <arramli@ucdavis.edu>
2023-04-23 12:14:24 -0700

    Update install2.sh

--- sh/install2.sh
+++ sh/install2.sh
@@ -65,3 +65,5 @@ echo "sudo python "$PY_PATH"internet.py" >> $AUTO_FILE # in my 7 inch last time 
 echo "sudo python "$PY_PATH"sensor.py" >> $AUTO_FILE
 echo "sudo sh "$SH_PATH"Chromium_Open.sh" >> $AUTO_FILE  # in the 3.5 and 7 screen remove "sudo" cuz chrome requires that
 #echo "python "$PY_PATH"door.py" >> $AUTO_FILE
+echo "sudo python "$PY_PATH"door.py" >> $AUTO_FILE
+
