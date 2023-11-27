1bae26b8487a12712bb895885a3298f399c0c3ef
Albara Ramli <arramli@ucdavis.edu>
2023-11-26 18:28:45 -0800

    Update Chromium_Open.sh

--- sh/Chromium_Open.sh
+++ sh/Chromium_Open.sh
@@ -22,9 +22,15 @@ echo ${MACX2} > "${DATA_PATH}sbr.sbr"
 ###
 echo "1" > "$DATA_PATH"chrome_status.txt
 #Open Chromium Browser Instance, if the device is not mic
-if [ "$(sed '3q;d' "$CONFIG_FILE")" != 'mic' ]; then
+if [ "$(sed '3q;d' "$CONFIG_FILE")" = 'monitor' ]; then
 #echo "$(sed '4q;d' "$CONFIG_FILE")screen.php?size=$(sed '5q;d' "$CONFIG_FILE")&icdx=1&city=$(sed '1q;d' "$CONFIG_FILE")&mac=${MACX2}"
 chromium-browser --kiosk --disable-gpu --incognito --no-sandbox --noerrdialogs -disable-session-crashed-bubble --disable-infobars --test-type --check-for-update-interval=31536000 "$(sed '4q;d' "$CONFIG_FILE")screen.php?size=$(sed '5q;d' "$CONFIG_FILE")&icdx=1&city=$(sed '1q;d' "$CONFIG_FILE")&mac=${MACX2}" #(for 3.5 screen to run as root) --user-data-dir=/home/pi
 fi
 #for door
-#chromium-browser --kiosk --disable-gpu --incognito --no-sandbox --noerrdialogs -disable-session-crashed-bubble --disable-infobars --test-type --check-for-update-interval=31536000 "$(sed '4q;d' "$CONFIG_FILE")pi/swipe-door.php?sidex=men&size=$(sed '5q;d' "$CONFIG_FILE")&icdx=1&city=$(sed '1q;d' "$CONFIG_FILE")&mac=${MACX2}" #(for 3.5 screen to run as root) --user-data-dir=/home/pi
+if [ "$(sed '3q;d' "$CONFIG_FILE")" = 'door' ]; then
+chromium-browser --kiosk --disable-gpu --incognito --no-sandbox --noerrdialogs -disable-session-crashed-bubble --disable-infobars --test-type --check-for-update-interval=31536000 "$(sed '4q;d' "$CONFIG_FILE")pi/swipe-door.php?sidex=men&size=$(sed '5q;d' "$CONFIG_FILE")&icdx=1&city=$(sed '1q;d' "$CONFIG_FILE")&mac=${MACX2}" #(for 3.5 screen to run as root) --user-data-dir=/home/pi
+fi
+#for ligh
+if [ "$(sed '3q;d' "$CONFIG_FILE")" = 'door' ]; then
+chromium-browser --kiosk --disable-gpu --incognito --no-sandbox --noerrdialogs -disable-session-crashed-bubble --disable-infobars --test-type --check-for-update-interval=31536000 "$(sed '4q;d' "$CONFIG_FILE")pi/index.php?sidex=men&size=$(sed '5q;d' "$CONFIG_FILE")&icdx=1&city=$(sed '1q;d' "$CONFIG_FILE")&mac=${MACX2}" #(for 3.5 screen to run as root) --user-data-dir=/home/pi
+fi
