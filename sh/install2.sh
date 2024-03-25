904c06c741ba42fc6b4fc1b593caa8ca0e0359e0
Albara Ramli <arramli@ucdavis.edu>
2024-03-25 15:50:28 -0700

    Update install2.sh

--- sh/install2.sh
+++ sh/install2.sh
@@ -64,7 +64,7 @@ echo "sudo python "$PY_PATH"mic_listen_online.py" >> $AUTO_FILE
 echo "sudo python "$PY_PATH"internet.py" >> $AUTO_FILE # in my 7 inch last time works only when there is no sudo here
 #echo "sudo python "$PY_PATH"sensor.py" >> $AUTO_FILE
 echo "sudo python "$PY_PATH"tf-mini_plus-new.py" >> $AUTO_FILE
-echo "python "$PY_PATH"switch.py" >> $AUTO_FILE
+echo "sudo python "$PY_PATH"switch.py" >> $AUTO_FILE
 echo "python "$PY_PATH"light.py" >> $AUTO_FILE
 echo "sudo sh "$SH_PATH"Chromium_Open.sh" >> $AUTO_FILE  # in the 3.5 and 7 screen remove "sudo" cuz chrome requires that
 echo "python "$PY_PATH"door.py" >> $AUTO_FILE
