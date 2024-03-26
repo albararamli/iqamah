f4580d3198917c7e83136ba569dd62e3f296100e
Albara Ramli <arramli@ucdavis.edu>
2024-03-26 12:22:45 -0700

    Update switch.py

--- py/switch.py
+++ py/switch.py
@@ -1,48 +1,38 @@
 import os
-from fun_sbr import *
-import RPi.GPIO as GPIO
-GPIO.setwarnings(False) 
-
-import time
 import sys
+import time
+import RPi.GPIO as GPIO
+from fun_sbr import *
 
-#exit the file if the device is not a mic device
-if(device_type!='mic'):
-    sys.exit() 
-        
-dir_local=path_sh
-dir_local_mic=path_py
+GPIO.setwarnings(False)
 
-status_web=-1 #blue led status 0,1
-file_web=path_data+'mic_status_web.txt'
-#status_local=0 #green led status 0,1
-file_relay=path_data+'mic_status_relay.txt'
+# Exit the file if the device is not a mic device
+if device_type != 'mic':
+    sys.exit()
+
+dir_local = path_sh
+dir_local_mic = path_py
+
+status_web = -1  # blue led status 0,1
+file_web = path_data + 'mic_status_web.txt'
+file_relay = path_data + 'mic_status_relay.txt'
 
 GPIO.setmode(GPIO.BOARD)
 
 print("Progress")
 
-GPIO.setup(LED_RED,GPIO.OUT)
-GPIO.setup(LED_GREEN,GPIO.OUT)
-GPIO.setup(LED_YELLOW,GPIO.OUT)
+GPIO.setup(LED_RED, GPIO.OUT)
+GPIO.setup(LED_GREEN, GPIO.OUT)
+GPIO.setup(LED_YELLOW, GPIO.OUT)
 
 GPIO.setup(SWITCH_YELLOW, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(SWITCH_RED, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-status_web= get_it(file_web)
-
+status_web = get_it(file_web)
 
 # Function to check switch state
 def check_yellow_switch():
     if GPIO.input(SWITCH_YELLOW) == GPIO.LOW:
-        #if(status_web==1):
-        sys.argv = ['0','WEB_OFF']
-        runx(dir_local_mic+'mic_control.py')
-        time.sleep(0.1)
-        ###
-        sys.argv = ['0','RELAY_OFF']
-        runx(dir_local_mic+'mic_control.py')
-        time.sleep(0.1)
         return "Pressed"
     else:
         return "Not Pressed"
@@ -50,24 +40,60 @@ def check_yellow_switch():
 # Function to check switch state
 def check_red_switch():
     if GPIO.input(SWITCH_RED) == GPIO.LOW:
-        #if(status_web==1):
-        sys.argv = ['0','WEB_OFF']
-        runx(dir_local_mic+'mic_control.py')
-        time.sleep(0.1)
-        ###
-        sys.argv = ['0','RELAY_ON']
-        runx(dir_local_mic+'mic_control.py')
-        time.sleep(0.1)
         return "Pressed"
     else:
         return "Not Pressed"
 
-
 # Main function to test the switch
 
+start_time_yellow = None
+start_time_red = None
 while True:
     try:
         yellow_state = check_yellow_switch()
         red_state = check_red_switch()
-    except:
-        pass
+
+        # Check if yellow button is pressed
+        if yellow_state == "Pressed":
+            if start_time_yellow is None:
+                start_time_yellow = time.time()  # Start counting time
+            elif time.time() - start_time_yellow >= 2:  # Check if 2 seconds elapsed
+                print("Yellow button pressed continuously for 2 seconds.")
+                #################################################
+                sys.argv = ['0', 'WEB_OFF']
+                runx(dir_local_mic + 'mic_control.py')
+                time.sleep(0.1)
+                sys.argv = ['0', 'RELAY_OFF']
+                runx(dir_local_mic + 'mic_control.py')
+                time.sleep(0.1)
+                print("YELLOW PRESSED")
+                #################################################
+                start_time_yellow = None  # Reset start time
+                # Do whatever you need to do when yellow button is pressed for 10 seconds
+
+        else:
+            start_time_yellow = None  # Reset start time if button is released
+
+        # Check if red button is pressed
+        if red_state == "Pressed":
+            if start_time_red is None:
+                start_time_red = time.time()  # Start counting time
+            elif time.time() - start_time_red >= 2:  # Check if 2 seconds elapsed
+                print("Red button pressed continuously for 2 seconds.")
+                ##################################################
+                sys.argv = ['0', 'WEB_OFF']
+                runx(dir_local_mic + 'mic_control.py')
+                time.sleep(0.1)
+                sys.argv = ['0', 'RELAY_ON']
+                runx(dir_local_mic + 'mic_control.py')
+                time.sleep(0.1)
+                print("RED PRESED")
+                ##################################################
+                start_time_red = None  # Reset start time
+                # Do whatever you need to do when red button is pressed for 10 seconds
+
+        else:
+            start_time_red = None  # Reset start time if button is released
+
+    except Exception as e:
+        print("Error occurred:", e)
